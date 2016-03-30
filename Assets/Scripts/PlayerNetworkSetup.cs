1bb49184710f04c78b18fbc08819ae56fdb1d026
unknown <jamie44344@gmail.com>
2016-03-30 15:27:58 -0400

    Networking is up

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            //disable the main camera
+            GameObject.Find("SceneCamera").SetActive(false);
+            //enable the player script which will enable the player camera
+            GetComponent<Player>().enabled = true;
+        }
+	}
+}
